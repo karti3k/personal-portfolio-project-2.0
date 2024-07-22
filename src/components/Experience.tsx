50a8d2f594c6739102662edc921e4f051143669e
Kartik <kartikbhatia2304@gmail.com>
2024-07-22 13:54:18 +0530

    completed experience component...

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,9 @@
 'use client';
-import KodersLogo from '@/assets/Koders_logo.jpg'
 import React, { useState } from 'react';
+import Image, { StaticImageData } from 'next/image';
+import KodersLogo from '@/assets/Koders_logo.jpg';
+import GrowcifyLogo from '@/assets/growcify_logo.jpg';
+import YtChannelLogo from '@/assets/exams_result_yt_channel_logo.jpg';
 
 interface ExperienceProps {
   isDarkMode: boolean;
@@ -8,7 +11,7 @@ interface ExperienceProps {
 
 // Interface for experience items
 interface ExperienceItem {
-  companyLogo: string;
+  companyLogo: StaticImageData;
   companyName: string;
   title: string;
   duration: string;
@@ -19,52 +22,37 @@ interface ExperienceItem {
 // Array of experience items
 const experiences: ExperienceItem[] = [
   {
-    companyLogo: 'KodersLogo',
-    companyName: 'Mirror Company',
-    title: 'Frontend Developer Trainee',
+    companyLogo: KodersLogo,
+    companyName: 'Koders',
+    title: 'Frontend Dev Trainee',
     duration: 'Jun 2024 - Aug 2024',
     details: [
-      'Developed user-friendly web applications using React and Next.js.',
-      'Collaborated with the design team to implement responsive UI designs.',
-      'Optimized web applications for performance and scalability.',
-      'Implemented Tailwind CSS for consistent and modular styling.'
+      'Worked as part of a team of three under the supervision of a lead, ensuring tasks were completed within the deadline.',
+      'Participated in weekly meetings with mentors to receive feedback and discuss work progress, where each team member presented their work and addressed any questions.',
+      'Designed wireframes using Figma and developed the frontend using Next.js, Tailwind CSS, and TypeScript, ensuring the implementation closely matched the wireframe designs.'
     ],
-    certificate: '/path/to/certificate1.png' // Optional
+    // certificate: '/path/to/certificate1.png' // Optional
   },
   {
-    companyLogo: '/path/to/logo2.png',
-    companyName: 'Another Company',
+    companyLogo: GrowcifyLogo,
+    companyName: 'Growcify',
     title: 'Graphic Design Intern',
-    duration: 'Jan 2024 - May 2024',
+    duration: 'Feb 2024 - Apr 2024',
     details: [
-      'Created visual content for social media and marketing campaigns.',
-      'Collaborated with the marketing team to design promotional materials.',
-      'Ensured brand consistency across all visual assets.',
-      'Worked on video editing projects for promotional content.'
+      'Developed and executed a weekly design plan for Growcify\'s social media platform, creating engaging posts and reels using Canva.',
+      'Ensured designs adhered to company objectives, brand guidelines, and effectively communicated Growcify\'s SaaS ecommerce platform benefits to small and medium-sized businesses.',
+      'Worked closely with the co-founder to gather feedback and refine design concepts, ensuring quality and consistency.',
     ]
   },
   {
-    companyLogo: '/path/to/logo3.png',
-    companyName: 'Tech Solutions',
-    title: 'Software Engineer Intern',
-    duration: 'Sep 2023 - Dec 2023',
+    companyLogo: YtChannelLogo,
+    companyName: 'Exams Result YT',
+    title: 'Video Editor',
+    duration: 'Jul 2021 - Sep 2021',
     details: [
-      'Assisted in the development of backend services using Node.js.',
-      'Participated in code reviews and provided constructive feedback.',
-      'Wrote unit tests to ensure code quality and reliability.',
-      'Contributed to the creation of technical documentation.'
-    ]
-  },
-  {
-    companyLogo: '/path/to/logo4.png',
-    companyName: 'Creative Agency',
-    title: 'Junior Web Developer',
-    duration: 'Jun 2023 - Aug 2023',
-    details: [
-      'Designed and implemented responsive websites using HTML, CSS, and JavaScript.',
-      'Worked with clients to understand their requirements and deliver solutions.',
-      'Integrated third-party APIs to add functionality to web applications.',
-      'Maintained and updated existing websites to improve performance.'
+      'Created videos on government exams, including application processes and eligibility, for a YouTube channel.',
+      'Worked closely with creator, incorporating feedback and making adjustments to meet their specific requirements.',
+      'Gained skills in video editing, graphic design, and professional voiceover.'
     ]
   }
 ];
@@ -81,81 +69,42 @@ const Experience: React.FC<ExperienceProps> = ({ isDarkMode }) => {
   return (
     <section id="experience-section" className={`py-8 px-8 md:px-12 lg:px-32 flex flex-col justify-center items-center md:items-start md:justify-start md:flex-col ${isDarkMode ? 'bg-custom-black' : 'bg-custom-blue'} jua-font`}>
       <h2 className={`jua-font text-4xl md:text-5xl pb-8 opacity-10 md:h-20 drop-shadow-lg ${isDarkMode ? 'text-custom-green' : 'text-white'}`}>
-                Experience:
-            </h2>
+        Experience:
+      </h2>
 
-      <div className="flex flex-col md:flex-row md:space-x-9 w-full">
-        
-        {/* Render experience items in two columns */}
-        <div className="md:w-1/2 space-y-4">
-          {experiences.slice(0, 2).map((experience, index) => (
-            <div key={index} className="w-full space-y-4">
-              <li className={`list-none py-4 px-2 rounded-xl ${isDarkMode ? 'bg-custom-black border border-custom-blue-green text-custom-blue-green' : 'bg-custom-blue border border-white text-white'}`}>
-                
-                {/* Button to toggle experience item open/close */}
-                <button
-                  className="flex justify-between items-center w-full font-normal"
-                  onClick={() => toggleExperience(index)}
-                >
-                  <div className="flex items-center justify-center space-x-4">
-                  <img src={experience.companyLogo} alt={`logo`} className="w-10 h-10 object-contain" />
+      <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between w-full space-y-4 md:space-y-0">
+        {experiences.map((experience, index) => (
+          <div key={index} className="w-full md:w-[30%] space-y-4">
+            <li className={`list-none py-4 px-2 rounded-xl ${isDarkMode ? 'bg-custom-black border border-custom-blue-green text-custom-blue-green' : 'bg-custom-blue border border-white text-white'}`}>
+              
+              {/* Button to toggle experience item open/close */}
+              <button
+                className="flex justify-between items-center w-full font-normal"
+                onClick={() => toggleExperience(index)}
+              >
+                <div className="flex items-center justify-center px-2">
+                  <Image src={experience.companyLogo} alt={`${experience.companyName} logo`} width={40} height={40} className="object-contain pr-2" />
                   <span>{experience.companyName}</span>
-                  </div>
-                  <span className='px-4'>{experience.title}</span>
-                  <span className="ml-2">{openIndex === index ? '-' : '+'}</span>
-                </button>
-
-                {/* Section to expand/collapse experience details */}
-                <div className={`overflow-hidden transition-all ${openIndex === index ? 'max-h-full' : 'max-h-0'}`}>
-                <span className="text-xs">{experience.duration}</span>
-                  <ul className="list-disc p-4 space-y-2 anekDevanagari-font text-sm">
-                    {experience.details.map((detail, i) => (
-                      <li key={i}>{detail}</li>
-                    ))}
-                  </ul>
-                  {experience.certificate && (
-                    <img src={experience.certificate} alt="Certificate" className="mt-4 w-full object-contain rounded-md" />
-                  )}
                 </div>
-              </li>
-            </div>
-          ))}
-        </div>
+                <span className='px-4'>{experience.title}</span>
+                <span className="ml-2">{openIndex === index ? '-' : '+'}</span>
+              </button>
 
-        <div className="md:w-1/2 space-y-4 mt-4 md:mt-0">
-          {experiences.slice(2).map((experience, index) => (
-            <div key={index + 2} className="w-full space-y-4">
-              <li className={`list-none py-4 px-2 rounded-xl ${isDarkMode ? 'bg-custom-black border border-custom-blue-green text-custom-blue-green' : 'bg-custom-blue border border-white text-white'}`}>
-                
-                {/* Button to toggle experience item open/close */}
-                <button
-                  className="flex justify-between items-center w-full font-normal"
-                  onClick={() => toggleExperience(index + 2)}
-                >
-                  <div className="flex items-center justify-center space-x-4">
-                  <img src={experience.companyLogo} alt={`logo`} className="w-10 h-10 object-contain" />
-                  <span>{experience.companyName}</span>
-                  </div>
-                  <span>{experience.title}</span>
-                  <span className="ml-2">{openIndex === index + 2 ? '-' : '+'}</span>
-                </button>
-
-                {/* Section to expand/collapse experience details */}
-                <div className={`overflow-hidden transition-all ${openIndex === index + 2 ? 'max-h-full' : 'max-h-0'}`}>
-                <span className="text-xs">{experience.duration}</span>
-                  <ul className="list-disc p-4 space-y-2 anekDevanagari-font text-sm">
-                    {experience.details.map((detail, i) => (
-                      <li key={i}>{detail}</li>
-                    ))}
-                  </ul>
-                  {experience.certificate && (
-                    <img src={experience.certificate} alt="Certificate" className="mt-4 w-full object-contain rounded-md" />
-                  )}
-                </div>
-              </li>
-            </div>
-          ))}
-        </div>
+              {/* Section to expand/collapse experience details */}
+              <div className={`overflow-hidden transition-all ${openIndex === index ? 'max-h-full' : 'max-h-0'}`}>
+                <span className="text-xs pl-2">{experience.duration}</span>
+                <ul className="list-disc p-4 space-y-2 anekDevanagari-font text-sm">
+                  {experience.details.map((detail, i) => (
+                    <li key={i}>{detail}</li>
+                  ))}
+                </ul>
+                {/* {experience.certificate && (
+                  <Image src={experience.certificate} alt="Certificate" width={600} height={400} className="mt-4 w-full object-contain rounded-md" />
+                )} */}
+              </div>
+            </li>
+          </div>
+        ))}
       </div>
     </section>
   );
